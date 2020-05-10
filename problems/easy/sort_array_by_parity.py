465a6ee000186da4f8e188184e907ac2481795c7
Kris Anaya <krismanaya@gmail.com>
2020-05-10 15:52:53 -0700

    refactor, sort in place

--- problems/easy/sort_array_by_parity.py
+++ problems/easy/sort_array_by_parity.py
@@ -6,6 +6,11 @@ def sortArrayByParity(A: List[int]) -> List[int]:
     return an array consisting of all the even elements of A, 
     followed by all the odd elements of A.
     """
-    evenArray = list(filter(lambda x: x % 2 == 0, A))
-    oddArray = list(filter(lambda x: x % 2 != 0, A))
-    return evenArray + oddArray
+    p0 = 0
+    for p1, _ in enumerate(A):
+        if A[p0] % 2 != 0 and A[p1] % 2 != 0:
+            continue
+        else:
+            A[p0], A[p1] = A[p1], A[p0]
+            p0 += 1
+    return A
